47f0bdcb99e38c59418a30c925329b3ad9f7a7f1
David Serfaty <davidserfaty11@gmail.com>
2024-11-17 20:20:54 +0200

    code fixed for timecalc

--- TimeCalc.java
+++ TimeCalc.java
@@ -1,15 +1,16 @@
 public class TimeCalc {
+    final static int MINUTESINHOUR = 60;
     public static void main(String[] args) {
-       final int minutesInHour = 60;
-    
-        String time  = (args[0]);
-        int minutesToAdd  = Integer.parseInt(args[1]);
+       
+            String time  = (args[0]);
+            int minutesToAdd  = Integer.parseInt(args[1]);
 
-        int i = 0;
-                String hoursInput   = ""; 
-                String minutesInput = "";
+            int i = 0;
+            String hoursInput   = ""; 
+            String minutesInput = "";
         
-        while ( i < 5) {
+        while ( i < 5) 
+            {
                 char n = time.charAt(i);
 
                 if (i < 2) { hoursInput = hoursInput + n; }
@@ -18,24 +19,22 @@ public class TimeCalc {
                 i++ ;
             }
 
-                int hoursInt = Integer.parseInt(hoursInput);
-                int minutesInt = Integer.parseInt(minutesInput);
+            int hoursInt = Integer.parseInt(hoursInput);
+            int minutesInt = Integer.parseInt(minutesInput);
                
-        int totalMinutes = 
-                (hoursInt * minutesInHour) + minutesInt + minutesToAdd;
+            int totalMinutes = 
+                (hoursInt * MINUTESINHOUR) + minutesInt + minutesToAdd;
 
-        int totalHours = totalMinutes / minutesInHour;
-        int newHours = totalHours % 24;
-        int newMinutes = totalMinutes - (totalHours * minutesInHour);
+            int totalHours = totalMinutes / MINUTESINHOUR;
+            int newHours = totalHours % 24;
+            int newMinutes = totalMinutes - (totalHours * MINUTESINHOUR);
 
-        String newHouString = String.valueOf(newHours);
-        String newMinutesString = String.valueOf(newMinutes);
+            String newHouString = String.valueOf(newHours);
+            String newMinutesString = String.valueOf(newMinutes);
 
-        if (newHours < 12) { 
-            newHouString = ("0" + newHouString); }
+        if (newHours < 10) { newHouString = ("0" + newHouString); }
 
-        if (newMinutes < 10 )  {
-            newMinutesString = "0" + newMinutesString; }  
+        if (newMinutes < 10 )  { newMinutesString = "0" + newMinutesString; }  
 
         System.out.println(newHouString + ":" + newMinutesString);    
         
